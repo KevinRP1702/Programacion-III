b36e703f02220772874ac504b4ce2702c322fecf
KevinRP1702 <159939479+KevinRP1702@users.noreply.github.com>
2024-02-20 10:43:25 -0600

    Unidad 2: ejercicio 13

--- ExampleTM/src/Ventana.java
+++ ExampleTM/src/Ventana.java
@@ -1,19 +1,57 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.print.attribute.AttributeSetUtilities;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Ventana {
+public class Ventana extends JFrame{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		JFrame ventana = new JFrame("Prueba");
-		ventana.setVisible(true);
-		ventana.setSize(600,800);
-		ventana.setLocation(300,100);
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.setResizable(false);
+	public Ventana() {
+			this.setVisible(true);
+			this.setSize(1000,500);
+			this.setLocation(200,200);
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setResizable(true);
+			this.setTitle("Mi Ventana");
+			this.setMinimumSize(new Dimension(250,250));
+			this.setMaximumSize(new Dimension(1000,750));
+			this.setLocationRelativeTo(null);
+			
+			this.setLayout(null);
+			
+			this.IniciarComponentes();
+		}
+	
+	public void IniciarComponentes() {
 		
-		//Centrar en la pantalla
-		ventana.setLocationRelativeTo(null);
+		JPanel login = new JPanel();
+		login.setSize(this.getWidth()/2, this.getHeight());
+		login.setBackground(Color.gray);
+		login.setLayout(null);
+		this.add(login);
+		
+		JLabel tituloLogin = new JLabel("Acceder", 0);
+		tituloLogin.setSize(300, 80);
+		tituloLogin.setLocation(100,20);
+		tituloLogin.setOpaque(true);
+		tituloLogin.setFont(new Font ("Agency FB", Font.BOLD, 40));
+		tituloLogin.setBackground(Color.red);
+		tituloLogin.setForeground(Color.white);
+		login.add(tituloLogin);
+		
+		/*
+		JPanel registro = new JPanel();
+		registro.setSize(this.getWidth()/2, this.getHeight());
+		registro.setBackground(Color.GREEN);
+		registro.setLocation(500,0);
+		this.add(registro);
+		*/
+		//Size
+		//Location
+		//Background - opaque
+		//String constructor
 	}
-
 }
