9b225b5b0ffbe1e674bc98d688bce4bcccc38bbb
KevinRP1702 <159939479+KevinRP1702@users.noreply.github.com>
2024-02-28 13:42:36 -0600

    Unidad 2: ejercicio 24

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,25 +1,47 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 
+import javax.print.attribute.AttributeSetUtilities;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.LineBorder;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.LayoutFocusTraversalPolicy;
+import javax.swing.border.Border;
 
 public class Ventana extends JFrame{
 public Ventana() {
 
 			//Atributos de la ventana
 			this.setVisible(true);
-			this.setSize(1000,800);
+			this.setSize(1000,750);
 			this.setLocation(200,200);
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setResizable(true);
-			this.setTitle("User Login");
+			this.setTitle("Mi Ventana");
 			this.setMinimumSize(new Dimension(250,250));
 			this.setMaximumSize(new Dimension(1000,750));
 			this.setLocationRelativeTo(null);
@@ -32,128 +54,60 @@ public Ventana() {
 	//Metodo para los componentes
 	public void IniciarComponentes() {
 
-		this.login();
+	
+		this.calculadora();
+		//this.registro();
+		//this.admin();
+		this.repaint();	
 	}
 	
-	public void login() {
+	public void calculadora() {
+		this.setSize(480,650);
 		
-		//Primer panel
-		JPanel login = new JPanel();
-		login.setSize(this.getWidth(), this.getHeight());
-		login.setBackground(Color.decode("#154c79"));
-		login.setLayout(null);
-		this.add(login);
+		JPanel panel = new JPanel();
+		panel.setSize(this.getWidth(), this.getHeight());
+		panel.setBackground(Color.decode("#35E4F1"));
+		panel.setLayout(new BorderLayout());
 		
-		//Primer panel
+		JLabel text = new JLabel("100.00", 4);
+		text.setOpaque(true);
+		text.setFont(new Font ("Agency FB", Font.CENTER_BASELINE, 40));
+		panel.add(text, BorderLayout.NORTH);
+		
 		JPanel centro = new JPanel();
-		centro.setBounds(100,100,800,550);
-		centro.setBackground(Color.decode("#063970"));
-		centro.setBorder(new BevelBorder(BevelBorder.RAISED));
-		centro.setLayout(null);
-		login.add(centro);
+		centro.setBackground(Color.decode("#CF7F54"));
+		centro.setLayout(new GridLayout(4,3));
+		panel.add(centro, BorderLayout.CENTER);
 		
-		//Etiqueta titulo
-		JLabel tituloLogin = new JLabel("User Login", 0);
-		tituloLogin.setBounds(350, 10, 300, 80);
-		//tituloLogin.setOpaque(true);
-		tituloLogin.setFont(new Font ("Agency FB", Font.CENTER_BASELINE, 50));
-		tituloLogin.setBackground(Color.CYAN);
-		tituloLogin.setForeground(Color.white);
-		login.add(tituloLogin);
 		
-		//Etiqueta titulo
-		JLabel lblMiCuenta = new JLabel("My Account", 0);
-		lblMiCuenta.setBounds(250, 60, 300, 80);
-		//tituloLogin.setOpaque(true);
-		lblMiCuenta.setFont(new Font ("Agency FB", Font.CENTER_BASELINE, 70));
-		lblMiCuenta.setBackground(Color.CYAN);
-		lblMiCuenta.setForeground(Color.decode("#d4c58c"));
-		centro.add(lblMiCuenta);
+		String btns[] = {"7", "8", "9", "6", "5", "4", "3", "2", "1", "0", ".", "/"};
+		for(int i = 0; i<12; i++) {
+			JButton boton = new JButton(btns[i]);
+			centro.add(boton);
+		}
 		
-		//Etiqueta titulo
-		JLabel lblUser = new JLabel("Enter your username");
-		lblUser.setBounds(150, 160, 200, 80);
-		//lblUser.setOpaque(true);
-		lblUser.setFont(new Font ("Agency FB", Font.CENTER_BASELINE, 25));
-		lblUser.setBackground(Color.CYAN);
-		lblUser.setForeground(Color.white);
-		centro.add(lblUser);
+		JPanel east = new JPanel();
+		east.setBackground(Color.decode("#98E055"));
+		east.setLayout(new GridLayout(3,1));
+		panel.add(east, BorderLayout.EAST);
 		
-		//Etiqueta titulo
-		JLabel lblTextoUser = new JLabel("", 0);
-		lblTextoUser.setBounds(150, 240, 500, 50);
-		lblTextoUser.setOpaque(true);
-		lblTextoUser.setFont(new Font ("Agency FB", Font.CENTER_BASELINE, 25));
-		lblTextoUser.setBackground(Color.white);
-		lblTextoUser.setForeground(Color.white);
-		lblTextoUser.setBorder(new LineBorder(Color.decode("#03F0FF"),2,true));
-		centro.add(lblTextoUser);
+		String btns2[] = {"+", "-", "="};
+		for(int i = 0; i<3; i++) {
+			JButton boton = new JButton(btns2[i]);
+			east.add(boton);
+		}
 		
-		JLabel lblImgUser = new JLabel();
-		lblImgUser.setIcon(new ImageIcon(getClass().getResource("UserIcon.png")));
-		lblImgUser.setBounds(107, 240, 50, 50);
-		centro.add(lblImgUser);
+		JPanel west = new JPanel();
+		west.setBackground(Color.DARK_GRAY);
+		west.setLayout(new BoxLayout(west, BoxLayout.Y_AXIS));
+		panel.add(west, BorderLayout.WEST);
 		
-		JLabel lblFondoImgUser = new JLabel();
-		lblFondoImgUser.setBounds(100, 245, 40, 40);
-		lblFondoImgUser.setOpaque(true);
-		lblFondoImgUser.setBackground(Color.decode("#d9d0a0"));
-		centro.add(lblFondoImgUser);
+		String btns3[] = {"MC", "M+", "*"};
+		for(int i = 0; i<3; i++) {
+			JButton boton = new JButton(btns3[i]);
+			west.add(boton);
+		}
 		
-		//Etiqueta titulo
-		JLabel lblPassword = new JLabel("Enter your password");
-		lblPassword.setBounds(150, 300, 200, 80);
-		//lblUser.setOpaque(true);
-		lblPassword.setFont(new Font ("Agency FB", Font.CENTER_BASELINE, 25));
-		lblPassword.setBackground(Color.CYAN);
-		lblPassword.setForeground(Color.white);
-		centro.add(lblPassword);
-		
-		//Etiqueta titulo
-		JLabel lblTextoPassword = new JLabel("  PASSWORD");
-		lblPassword.setAlignmentX(LEFT_ALIGNMENT);
-		lblTextoPassword.setBounds(150, 380, 500, 50);
-		lblTextoPassword.setOpaque(true);
-		lblTextoPassword.setFont(new Font ("Agency FB", Font.CENTER_BASELINE, 25));
-		lblTextoPassword.setBackground(Color.white);
-		lblTextoPassword.setForeground(Color.gray);
-		centro.add(lblTextoPassword);
-
-		//label imagen contraseña
-		JLabel lblImgPassword = new JLabel();
-		lblImgPassword.setIcon(new ImageIcon(getClass().getResource("PasswordIcon.png")));
-		lblImgPassword.setBounds(107, 380, 50, 50);
-		centro.add(lblImgPassword);
-		
-		//label color de fondo
-		JLabel lblFondoImg = new JLabel();
-		lblFondoImg.setBounds(100, 385, 40, 40);
-		lblFondoImg.setOpaque(true);
-		lblFondoImg.setBackground(Color.decode("#d9d0a0"));
-		centro.add(lblFondoImg);
-
-		//Boton login
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setBounds(300,450,200,80);
-		btnLogin.setFont(new Font ("Agency FB", Font.BOLD, 30));
-		btnLogin.setBackground(Color.decode("#edd88b"));
-		centro.add(btnLogin);
-		
-		//label no tienes cuenta
-		JLabel lblSinCuenta = new JLabel("Don't have an account?");
-		lblSinCuenta.setBounds(430,660, 180, 40);
-		lblSinCuenta.setFont(new Font ("Agency FB", Font.BOLD, 20));
-		lblSinCuenta.setForeground(Color.white);
-		login.add(lblSinCuenta);
-		
-		//lbl crear cuenta
-		JButton lblCrearCuenta = new JButton("Sign Up");
-		lblCrearCuenta.setBounds(410,710,180, 60);
-		lblCrearCuenta.setFont(new Font ("Agency FB", Font.BOLD, 30));
-		lblCrearCuenta.setBackground(Color.decode("#edd88b"));
-		lblCrearCuenta.setForeground(Color.black);
-		login.add(lblCrearCuenta);
-
-		this.repaint();
+		this.add(panel);
 	}
 }
